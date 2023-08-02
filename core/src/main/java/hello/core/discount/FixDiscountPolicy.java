4163cc35b9d4e835f4a2efcc4ff999d521915cd9
asg5450 <112463487+asg5450@users.noreply.github.com>
2023-08-02 14:50:56 +0900

    할인과 주문 도메인 생성
    우선 정액할인제로 할인 생성
    예제 단순화를 위해 상품 객체는 만들지 않았음

---
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000;    // 1000원 정액할인제
+
+    // FixDiscountPolicy.discount(회원객체, 가격) 사용시 VIP면 1000원 할인
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return discountFixAmount;
+        }else{
+            return 0;
+        }
+    }
+}
