361c218ce9cfee2dab3b32134503a391f0c8d383
asg5450 <112463487+asg5450@users.noreply.github.com>
2023-08-03 10:51:23 +0900

    DIP를 지키면 NullPointerException
    DIP를 지키면 구현체가 없어서 NullPointerException 발생

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,7 +11,9 @@ public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // ↓
+    private DiscountPolicy discountPolicy; // DIP 위반하지 않기 위해 이렇게 변경하면 구현체가 없어서 NullPointerException 발생
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
